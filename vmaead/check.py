081c9e2d115e71ef9cde5b9c5d29a5769eb3bbe3
Charles <tuan.lda204929@sis.hust.edu.vn>
2023-05-19 03:49:16 +0700

    viddeo-mae

--- vmaead/check.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-import torch
-
-input = torch.rand(3, 16, 224, 224)
-print(input.shape)
-f = nn.Conv3d(
-    in_channels=3, out_channels=768, kernel_size=(2, 16, 16), stride=(2, 16, 16)
-)
-print(f(input).shape)
