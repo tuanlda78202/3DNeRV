465f2111ee063ef67ba4db0dc3d3488112738b44
Charles <tuan.lda204929@sis.hust.edu.vn>
2023-07-27 20:40:16 +0700

    hot-fix

--- src/trainer/nerv3d_trainer.py
+++ src/trainer/nerv3d_trainer.py
@@ -125,7 +125,7 @@ class NeRV3DTrainer(BaseTrainer):
         self.model.eval()
 
         with torch.no_grad():
-            random_num = random.randint(1, len(self.dataset))
+            random_num = random.randint(10, len(self.dataset) - 10)
 
             valid_data = (
                 self.dataset[random_num].unsqueeze(0).permute(0, 4, 1, 2, 3).cuda()
