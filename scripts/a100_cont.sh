43f71054a17b12e7145f4c36451ef0cb29e7183c
tuanlda78202 <tuanleducanh78202@gmail.com>
2023-08-03 13:38:01 +0700

    wip

--- scripts/a100_cont.sh
+++ scripts/a100_cont.sh
@@ -5,22 +5,35 @@ python src/model/macs.py
 
 ## FAILED VIDEOS
 # Jockey 12M
-python scripts/train.py --config config/uvg-1080p/12M/jockey.yaml
-python scripts/test.py --config config/uvg-1080p/12M/jockey.yaml --resume saved/models/jockey-1080p_12M/300e.pth
+CUDA_VISIBLE_DEVICES=1 python scripts/train.py --config config/uvg-1080p/12M/jockey.yaml
+CUDA_VISIBLE_DEVICES=1 python scripts/test.py --config config/uvg-1080p/12M/jockey.yaml --resume saved/models/jockey-1080p_12M/300e.pth
 
 # Bosphorus 6M
-python scripts/train.py --config config/uvg-1080p/6M/bosphorus.yaml
-python scripts/test.py --config config/uvg-1080p/6M/bosphorus.yaml --resume saved/models/bosphorus-1080p_6M/300e.pth
+CUDA_VISIBLE_DEVICES=0 python scripts/train.py --config config/uvg-1080p/6M/bosphorus.yaml
+CUDA_VISIBLE_DEVICES=0 python scripts/test.py --config config/uvg-1080p/6M/bosphorus.yaml --resume saved/models/bosphorus-1080p_6M/300e.pth
 
 # Jockey 6M
-python scripts/train.py --config config/uvg-1080p/6M/jockey.yaml
-python scripts/test.py --config config/uvg-1080p/6M/jockey.yaml --resume saved/models/jockey-1080p_6M/300e.pth
+CUDA_VISIBLE_DEVICES=1 python scripts/train.py --config config/uvg-1080p/6M/jockey.yaml
+CUDA_VISIBLE_DEVICES=1 python scripts/test.py --config config/uvg-1080p/6M/jockey.yaml --resume saved/models/jockey-1080p_6M/300e.pth
 
 # Bosphorus 3M
 python scripts/train.py --config config/uvg-1080p/3M/bosphorus.yaml
 python scripts/test.py --config config/uvg-1080p/3M/bosphorus.yaml --resume saved/models/bosphorus-1080p_3M/300e.pth
 
-## 7 video 3M (scripts/train-uvg-1080p/3m_300e.sh)
+## 6 video 3M (scripts/train-uvg-1080p/3m_300e.sh)
+
+* 12m
+* jockey
+* 6m
+* bos
+* jockey
+* 3m
+* bee
+* bos
+* jockey
+* yacht
+* shake
+* ready
 
 ## NNC Compression (scripts/compress-uvg-1080p)
 
